01635714443f6fd4c005d07f93db08ebce7935eb
Atchougo <atchougo@student.42lyon.fr>
2023-01-23 17:41:25 +0100

    micro main fonctionne avec /bin/

---
+++ srcs/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atchougo <atchougo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 15:40:11 by atchougo          #+#    #+#             */
+/*   Updated: 2023/01/23 17:13:42 by atchougo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+char *find_PATH(char **env)
+{
+    int i;
+    char *temp;
+
+    i = 0;
+    while (env[i])
+    {
+        temp = ft_strnstr(env[i], "PATH", 5);
+        // printf("[%s] temp : %s\n", __FUNCTION__, temp);
+        if (temp)
+            return (env[i]);
+        i++;
+    }
+    return (NULL);
+}
+
+int main(int argc, char **argv, char **env)
+{
+    if (execve(ft_strjoin("/bin/", argv[1]), &argv[1], env) != 0)
+        printf("Command not found\n");
+    return 0;
+}
