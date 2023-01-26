822cd78dc01ed185c3fe9013cc3da55e6ae91526
Atchougo <atchougo@student.42lyon.fr>
2023-01-26 22:11:44 +0100

    add DEBUG macro for printf

--- includes/minishell.h
+++ includes/minishell.h
@@ -17,8 +17,52 @@
 # include <sys/types.h>
 # include <errno.h>
 
+/**
+ * @brief USE as a debug printf 
+ *        x is the text to add with % as printf
+ * 
+ * @param x : has to be a char *
+ * @param ... : is variadic macros
+ */
+# define DEBUGv(x, ...) printf("[%s:%d] "x"\n",__FUNCTION__ ,__LINE__, __VA_ARGS__)
+# define DEBUG(x) printf("[%s:%d] "x"\n",__FUNCTION__ ,__LINE__)
+
+typedef enum s_redir {
+    in = 1, // ( < )
+    out,    // ( > )
+    append, // ( >> )
+    heredoc // ( << )
+}   t_redir;
+
+typedef enum s_quote {
+    no,
+    big, // ( < )
+    litlle,    // ( > )
+}   t_quote;
+
+typedef struct s_parsing {
+    char *key; //PATH=
+    char *value; // /bin/:/usr/bin
+    int affiche_env; // 0 ou 1
+    struct s_env *next;
+    t_quote quote;
+}    t_parsing;
+
+typedef struct s_env {
+    char *key; // PATH=
+    char *value; // /bin/:/usr/bin
+    int affiche_env; // 0 ou 1
+    struct s_env *next;
+}    t_env;
+
+typedef struct s_path {
+    char *path; // PATH=/bin/:/usr/bin
+    char **path_splitted; // [0] = "/bin/" [1] = "/usr/bin"
+}    t_path;
+
 typedef struct s_command {
-    char    **env;
+    t_env    *env;
+    t_path   path;
     // gérer les redirections j'ai pas encore d'idée de comment faire. 
     char    **parsed_line;
     char    *input;
