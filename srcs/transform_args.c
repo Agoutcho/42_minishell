1b2c262c31eb59685a7510e1ef4df479700efd46
atchougo <atchougo@student.42lyon.fr>
2023-02-22 02:19:09 +0100

    ajout de la transfo en char **args

---
+++ srcs/transform_args.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   transform_args.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atchougo <atchougo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 02:05:09 by atchougo          #+#    #+#             */
+/*   Updated: 2023/02/22 02:17:12 by atchougo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	from_list_to_char(t_data *data, long j)
+{
+	t_args	*temp;
+	int		args_counter;
+	int		i;
+
+	i = 0;
+	args_counter = 0;
+	temp = data->cmd[j].arg->first;
+	while (temp)
+	{
+		args_counter++;
+		temp = temp->next;
+	}
+	data->cmd[j].args = (char **)malloc(sizeof(char *) * (args_counter + 1));
+	if (!data->cmd[j].args)
+	{
+		big_free(data);
+		exit(1);
+	}
+	temp = data->cmd[j].arg->first;
+	while (temp)
+	{
+		data->cmd[j].args[i] = ft_strdup(temp->arg);
+		temp = temp->next;
+        i++;
+	}
+    data->cmd[j].args[i] = 0;
+}
+
+void	transform_args(t_data *data)
+{
+	long	j;
+
+    j = 0;
+    while (j < data->size_cmd_array)
+    {
+        if (data->cmd[j].arg)
+            from_list_to_char(data, j);
+        j++;
+    }
+}
