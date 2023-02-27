d0f523c001695e9887cfb53979278df2c176f4b3
Agoutcho <atchougo@student.42lyon.fr>
2023-02-28 00:59:12 +0100

    [libft] fix strncmp.c

--- libft/src/ft_strncmp.c
+++ libft/src/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nradal <nradal@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: atchougo <atchougo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 11:27:14 by nradal            #+#    #+#             */
-/*   Updated: 2021/11/11 12:45:33 by nradal           ###   ########lyon.fr   */
+/*   Updated: 2023/02/27 23:38:37 by atchougo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	i;
 
+	if (!s1 || !s2)
+		return (-1);
 	if (n == 0)
 		return (0);
 	i = 0;
