fc1560252b0819e8f477e82d772cde40ee20823a
nradal <no.radal1203@gmail.com>
2023-03-13 15:36:38 +0100

    echo -nnnnnnnnn fix

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atchougo <atchougo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nradal <nradal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 15:50:52 by nradal            #+#    #+#             */
-/*   Updated: 2023/03/11 22:01:04 by atchougo         ###   ########.fr       */
+/*   Updated: 2023/03/13 15:36:20 by nradal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+int	echo_valid_arg(char *arg);
 
 int	ft_echo(t_cmd_array *cmd)
 {
@@ -19,7 +21,7 @@ int	ft_echo(t_cmd_array *cmd)
 
 	i = 0;
 	option = false;
-	while (cmd->args && cmd->args[i] && !ft_strcmp(cmd->args[i], "-n"))
+	while (cmd->args && cmd->args[i] && echo_valid_arg(cmd->args[i]))
 	{
 		option = true;
 		i++;
@@ -34,4 +36,19 @@ int	ft_echo(t_cmd_array *cmd)
 	if (!option)
 		ft_putchar_fd('\n', STDOUT_FILENO);
 	return (set_g_exit_code(0, 1));
+}
+
+int	echo_valid_arg(char *arg)
+{
+	int	i;
+
+	i = 1;
+	if (ft_strncmp(arg, "-n", 2) == 0)
+	{
+		while (arg[i] == 'n')
+			i++;
+		if (arg[i] == '\0')
+			return (1);
+	}
+	return (0);
 }
